42c3e13506eee7481039488836efb06f43ea32d8
iiWair <thomasboundaoui91180@gmail.com>
2025-05-21 18:32:51 +0200

    🔒 Nettoyage de la branche develop : uniquement DB, instance, ODT et secret

--- app/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "clesecrete"
--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.mnjkqaykkrlatiokegux:1Adminpassword!2@aws-0-eu-west-3.pooler.supabase.com:5432/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "devkey"
