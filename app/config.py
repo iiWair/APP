ae8be7d28cd73add8f05533dc21edab01edbdb31
iiWair <thomasboundaoui91180@gmail.com>
2025-05-22 10:10:02 +0200

    Message clair décrivant les changements

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 # config.py
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:ThomasBpassword2025@db.mnjkqaykkrlatiokegux.supabase.co:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.mnjkqaykkrlatiokegux:ThomasBpassword2025@aws-0-eu-west-3.pooler.supabase.com:6543/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "clesecrete"
